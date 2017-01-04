de83edb63bb86e3ad1313ad28ebbf3ad2a6140f3
BeerOnBeard <repoguy156+github@gmail.com>
2017-01-04 21:50:11 +0200

    Adding ability for user to unselect a friend after they have selected them. Adding unit tests around friendsViewModel.

--- src/home/friendsViewModel.js
+++ src/home/friendsViewModel.js
@@ -12,23 +12,35 @@ function FriendsViewModel(constArgs) {
 
   self.friends = ko.observableArray();
   self.selectedFriend = ko.observable();
+  self.selectedGift = ko.observable();
+
   self.selectedFriend.subscribe(function(friend) {
+    if (!friend) {
+      return;
+    }
+
     self.service.getGiftsPromise(friend)
       .then(function(gifts) {
         friend.gifts(gifts);
       });
   });
 
-  self.selectedGift = ko.observable();
-
   self.selectFriend = function(friend) {
     if (self.selectedGift()) {
       self.selectedGift(undefined);
     }
 
+    var currentFriendId;
     if (self.selectedFriend()) {
+      currentFriendId = self.selectedFriend().id;
       self.selectedFriend().gifts.removeAll();
       self.selectedFriend().selected(false);
+    }
+
+    // If same friend was selected again, unselect friend and get out of here
+    if (currentFriendId && friend.id === currentFriendId) {
+      self.selectedFriend(undefined);
+      return;
     }
 
     friend.selected(true);
@@ -36,8 +48,16 @@ function FriendsViewModel(constArgs) {
   };
 
   self.selectGift = function(gift) {
+    var currentGiftId;
     if (self.selectedGift()) {
+      currentGiftId = self.selectedGift().id;
       self.selectedGift().selected(false);
+    }
+
+    // If the same gift was selected again, unselect gift and get out of here
+    if (currentGiftId && currentGiftId === gift.id) {
+      self.selectedGift(undefined);
+      return;
     }
 
     gift.selected(true)
---
+++ src/home/friendsViewModel.test.js
@@ -0,0 +1,105 @@
+var FriendsViewModel = require('./friendsViewModel');
+var ko = require('knockout');
+
+describe('FriendsViewModel', function(){
+  beforeEach(function(){
+    this.mockService = jasmine.createSpyObj('service', [ 'getGiftsPromise' ]);
+
+    this.getGiftsPromise = new Promise(function(resolve){ resolve([{ id: 1 }]); });
+
+    this.mockService.getGiftsPromise.and.returnValue(this.getGiftsPromise);
+    this.SUT = new FriendsViewModel({ service: this.mockService });
+  });
+
+  describe('when selecting a friend', function(){
+    beforeEach(function(done){
+      var mockGifts = ko.observableArray();
+      spyOn(mockGifts, 'removeAll');
+      this.mockFriend = { id: 1, selected: function(){}, gifts: mockGifts };
+      spyOn(this.mockFriend, 'selected');
+
+      this.SUT.selectFriend(this.mockFriend);
+      this.getGiftsPromise.then(function(){ done(); });
+    });
+
+    it('should set selected on the friend', function(){
+      expect(this.mockFriend.selected).toHaveBeenCalledWith(true);
+    });
+
+    it('should set selectedFriend', function(){
+      expect(this.SUT.selectedFriend()).toBe(this.mockFriend);
+    });
+
+    it('should get gifts for the friend', function(){
+      expect(this.mockFriend.gifts().length).toBe(1);
+    });
+    
+    describe('and another friend had already been selected', function(){
+      beforeEach(function(done){
+        var friendTheSecondGifts = ko.observableArray();
+        this.mockFriendTheSecond = { id: 2, selected: function(){}, gifts: friendTheSecondGifts };
+        spyOn(this.mockFriendTheSecond, 'selected');
+
+        this.SUT.selectFriend(this.mockFriendTheSecond);
+        this.getGiftsPromise.then(function(){ done(); });
+      });
+
+      it('should remove all loaded gifts from the first friend', function(){
+        expect(this.mockFriend.gifts.removeAll).toHaveBeenCalled();
+      });
+
+      it('should set selected to false on the frist friend', function(){
+        expect(this.mockFriend.selected).toHaveBeenCalledWith(false);
+      });
+    });
+
+    describe('and the same friend is selected again', function(){
+      beforeEach(function(done){
+        this.SUT.selectFriend(this.mockFriend);
+        this.getGiftsPromise.then(function(){ done(); });
+      });
+
+      it('should remove all gifts', function(){
+        expect(this.mockFriend.gifts.removeAll).toHaveBeenCalled();
+      });
+
+      it('should set selected to false', function(){
+        expect(this.mockFriend.selected).toHaveBeenCalledWith(false);
+      });
+
+      it('should set selectedFriend to undefined', function(){
+        expect(this.SUT.selectedFriend()).toBe(undefined);
+      });
+    });
+  });
+
+  describe('when selecting a gift', function(){
+    beforeEach(function(){
+      this.mockGift = { id: 1, selected: function(){} };
+      spyOn(this.mockGift, 'selected');
+      this.SUT.selectGift(this.mockGift);
+    });
+
+    it('should set selected to true', function(){
+      expect(this.mockGift.selected).toHaveBeenCalledWith(true);
+    });
+
+    it('should set selectedGift', function(){
+      expect(this.SUT.selectedGift()).toBe(this.mockGift);
+    });
+
+    describe('and it is selected again', function(){
+      beforeEach(function(){
+        this.SUT.selectGift(this.mockGift);
+      });
+
+      it('should set selected to false', function(){
+        expect(this.mockGift.selected).toHaveBeenCalledWith(false);
+      });
+
+      it('should set selectedGift to undefined', function(){
+        expect(this.SUT.selectedGift()).toBe(undefined);
+      });
+    });
+  });
+});
